308d8d52c8cf4606f2101cd0ba21ea230b679a2e
baosy <baosy0805@thundersoft.com>
2024-09-13 20:02:58 +0800

    2024/9/13 注册的头像选择功能

--- MyApplication7/app/src/main/java/com/example/myapplication/LoginActivity.java
+++ MyApplication7/app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -5,18 +5,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
-    private int CODE = 0x717;
+    private int CODE1 = 0x717;
+    private int CODE2 = 0x718;
+    private int[] imageId = new int[]{R.drawable.img1, R.drawable.img2, R.drawable.img3, R.drawable.img4,
+            R.drawable.img5, R.drawable.img6,R.drawable.img7, R.drawable.img8, R.drawable.img9};
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
         Button login = (Button) findViewById(R.id.login);
         Button register = (Button) findViewById(R.id.register);
+        Button select = (Button) findViewById(R.id.select);
         login.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -41,9 +46,6 @@ public class LoginActivity extends AppCompatActivity {
         });
 
 
-
-
-
         /*
         注册按钮--跳转到注册界面并返回数据
          */
@@ -52,7 +54,19 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v){
                 Intent intent = new Intent();
                 intent.setClass(LoginActivity.this, RegisterActivity.class);
-                startActivityForResult(intent,CODE);
+                startActivityForResult(intent,CODE1);
+            }
+        });
+        /*
+    头像跳转按钮---选择用户头像
+     */
+        select.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.setClass(LoginActivity.this, IndexActivity.class);
+                startActivityForResult(intent,CODE2);
             }
         });
     }
@@ -64,13 +78,17 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode,  Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode==CODE && resultCode==CODE){
+        if (requestCode==CODE1 && resultCode==CODE1){
             String user = data.getStringExtra("user");
             String pwd = data.getStringExtra("pwd");
             String cfm = data.getStringExtra("cfm");
             ((EditText) findViewById(R.id.username)).setText(user);
             ((EditText) findViewById(R.id.password)).setText(pwd);
             ((EditText) findViewById(R.id.confirm)).setText(cfm);
+        }
+        if (requestCode == CODE2 && resultCode == CODE2){
+            int index = data.getIntExtra("imageId",0);
+            ((ImageView) findViewById(R.id.image)).setImageResource(imageId[index]);
         }
     }
 }
